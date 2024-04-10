031116e7a185d875b40c85b4411012ac7f5888d7
nuratann <kadyrov.atan@gmail.com>
2024-04-10 18:29:50 +0600

    zustand store for user

---
+++ src/stores/useUserStore.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+type User = {
+    username: String
+    firstName: String
+    lastName: String
+    birthDay: Date
+    email: String
+    gender: Gender
+}
+
+enum Gender {
+    male,
+    female,
+    hz
+}  
+
+type UserActions = {
+    updateFirstName: (firstName: User['firstName']) => void
+    updateLastName: (lastName: User['lastName']) => void
+}
+
+const useUserStore = create<User & UserActions>((set) => ({
+    username: "",
+    firstName: "",
+    lastName: "",
+    birthDay: new Date(),
+    email: "",
+    gender: Gender.hz,
+    updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
+    updateLastName: (lastName) => set(() => ({ lastName: lastName })),
+  })) 
